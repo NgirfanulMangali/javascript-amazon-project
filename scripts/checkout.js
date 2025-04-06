e67fc3154db2f1b05bcbf371f3739528f1683697
NgirfanulMangali <qwertymetal0.com>
2025-04-07 05:48:48 +0700

    make "save" appear when clicking "update"

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -33,12 +33,13 @@ cart.forEach((cartItem) => {
         <div class="product-price">$${formatCurrency(matchingProduct.priceCents)}</div>
         <div class="product-quantity">
           <span> Quantity: <span class="quantity-label">${cartItem.quantity}</span></span>
-          <span class="update-quantity-link link-primary js-update-link"
+          <span class="update-quantity-link link-primary js-update-link "
           data-product-id="${matchingProduct.id}">
             Update
-          </span>
-          
-          <span class="delete-quantity-link link-primary js-delete-link"
+            </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary">Save</span>
+          <span class="delete-quantity-link link-primary js-delete-link "
           data-product-id="${matchingProduct.id}">
             Delete
           </span>
@@ -120,6 +121,11 @@ document.querySelectorAll('.js-update-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
+      
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.classList.add('is-editing-quantity');
       
     });
   });
