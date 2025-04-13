bc6db6c123c53446f9cb5ea4844e8ebc6629988c
NgirfanulMangali <metalqwerty0@gmail.com>
2025-04-13 09:59:36 +0700

    done exercise 17b

---
+++ data/car.js
@@ -0,0 +1,19 @@
+class Car {
+    brand;
+    model;
+
+    constructor(carDetails) {
+        this.brand = carDetails.brand;
+        this.model = carDetails.model;
+    }
+    
+    displayInfo() {
+    return `${this.brand} ${this.model}`;
+    }
+    
+}
+const car1 = new Car({ brand: 'Toyota', model: 'Corolla' });
+const car2 = new Car({ brand: 'Honda', model: 'Civic' });
+
+console.log(car1.displayInfo());
+console.log(car2.displayInfo());
