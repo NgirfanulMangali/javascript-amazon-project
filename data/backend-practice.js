4e14badaadfb1bcc40f9fb4b66387f42ab473b0a
NgirfanulMangali <metalqwerty0@gmail.com>
2025-04-14 08:56:52 +0700

    done

---
+++ data/backend-practice.js
@@ -0,0 +1,8 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener();
+
+
+xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.send();
+xhr.response
--- data/car.js
+++ data/car.js
@@ -1,6 +1,9 @@
 class Car {
     brand;
     model;
+    speed = 0;
+   isTrunkOpen = false;
+
 
     constructor(carDetails) {
         this.brand = carDetails.brand;
@@ -8,12 +11,46 @@ class Car {
     }
     
     displayInfo() {
-    return `${this.brand} ${this.model}`;
+    console.log(`${this.brand} ${this.model}, speed: ${this.speed} km/h`);
     }
-    
+    go() {
+        this.speed =+ 5;
+        if (this.speed > 200) {
+            this.speed = 200;
+        }
+        if (this.isTrunkOpen) {
+            this.speed = 0;
+            console.log('Cannot drive with trunk open');
+        }
+    }
+    brake() {
+        this.speed -= 5;
+        if (this.speed < 0) {
+            this.speed = 0;
+        }  
+    }
+    openTrunk() {
+        this.isTrunkOpen = true;
+    }
+    closeTrunk() {
+        this.isTrunkOpen = false;
+    }
 }
 const car1 = new Car({ brand: 'Toyota', model: 'Corolla' });
 const car2 = new Car({ brand: 'Honda', model: 'Civic' });
 
-console.log(car1.displayInfo());
-console.log(car2.displayInfo());
+console.log(car1);
+console.log(car2);
+
+car1.closeTrunk();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo(); // Toyota Corolla, speed: 15 km/h
+
+car2.displayInfo(); // Honda Civic, speed: 0 km/h
+car2.go();
+car2.brake();
+car2.brake();
+car2.displayInfo(); // Honda Civic, speed: 0 km/h
